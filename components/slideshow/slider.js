8f2e616050e92850088a2ede165ec1bd14c7b6b6
ahmed el <ahmedelmeskyny@gmail.com>
2022-02-19 21:04:33 +0100

    slider

---
+++ components/slideshow/slider.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Slide } from "react-slideshow-image";
+
+//styles
+import "react-slideshow-image/dist/styles.css";
+import styles from "./slider.module.scss";
+
+const properties = {
+  indicators: true,
+  arrows: false,
+  duration: 200000,
+};
+
+const Slider = () => {
+  return (
+    <div>
+      <Slide easing="ease" {...properties} style={{ display: "none" }}>
+        <div className={styles.eachSlide}>
+          <div className={styles.slide}>
+            <div className={styles.image} style={{ width: "280px" }}>
+              <img src="/magensao7.png" width="280px"></img>
+            </div>
+            <div className={styles.description}>
+              <span className={styles.date}>Février, 2022</span>
+              <h1 className={styles.title}>MagEnsao N°7 DU Février, 2022 </h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem
+                ipsum' will uncover many web sites still in their infancy...
+              </p>
+              <div className={styles.magazineLinks}>
+                <a className={styles.link1}>LIRE CETTE ÉDITION </a>
+                <a className={styles.link2}>VOIR TOUS LES NUMÉROS</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eachSlide}>
+          <div className={styles.slide}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: "url(/corona.jpg)" }}
+            ></div>
+            <div className={styles.description}>
+              <span className={styles.date}>Janvier, 2022</span>
+              <h1 className={styles.title}>
+                Variant Omicron BA.2: les chercheurs percent ses premiers
+                secrets{" "}
+              </h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem...
+              </p>
+              <div className={styles.magazineLinks}>
+                <a className={styles.link1}>LIRE L'ARTICLE </a>
+                <a className={styles.link2}>TOUS LES ARTICLES LIÉS</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.eachSlide}>
+          <div className={styles.slide}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: "url(/ai.jpg)" }}
+            ></div>
+            <div className={styles.description}>
+              <span className={styles.date}>DÉCEMBRE, 2022</span>
+              <h1 className={styles.title}>
+                Comment l’intelligence artificielle va changer nos vies
+              </h1>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less
+                normal distribution of letters, as opposed to using 'Content
+                here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem
+                Ipsum as their default model text, and a search for 'lorem...
+              </p>
+              <div className={styles.magazineLinks}>
+                <a className={styles.link1}>LIRE L'ARTICLE </a>
+                <a className={styles.link2}>TOUS LES ARTICLES LIÉS</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Slide>
+    </div>
+  );
+};
+
+export default Slider;
