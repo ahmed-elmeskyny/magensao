0490580cd04d7671c5f9d8f2e7ccd82ef55e1df7
ahmed el <ahmedelmeskyny@gmail.com>
2022-04-26 10:46:54 +0000

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import "../styles/globals.scss";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G - JG1Q0KFFN1`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-JG1Q0KFFN1', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
